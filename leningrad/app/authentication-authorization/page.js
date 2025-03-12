1ce71368d2d5d54f38b495796cc5033e14b84936
Григорий Малышев <grisha@MacBook-Air-Grigorij.local>
2025-03-12 15:27:53 +0300

    added eye which support check password

--- leningrad/app/authentication-authorization/page.js
+++ leningrad/app/authentication-authorization/page.js
@@ -2,56 +2,80 @@
 
 import React, { useState } from "react";
 import './authentication-authorization-style.css';
+import { Eye, EyeOff } from "lucide-react";
 
 const AuthenticationAuthorizationPage = () => {
-  // Изменили начальное значение на true для формы входа
-  const [isLoginMode, setIsLoginMode] = useState(true);
+  const [isLoginMode, setIsLoginMode] = useState(false);
 
   const BackgroundTransition = () => (
     <div className="background-transition" />
   );
 
-  const InputField = ({ label, type, id, name }) => (
+  const InputField = ({ label, type, id, name }) => {
+    const [showPassword, setShowPassword] = useState(false);
+    const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+  
+    const handleInputChange = (e) => {
+      if (type === "password") {
+        setIsPasswordVisible(e.target.value.length > 0);
+      }
+    };
+  
+    return (
     <div className="input-group">
       <label htmlFor={id}><span className='qw'>{label}</span></label>
-      <input type={type} id={id} name={name} required />
+      
+      <div className="input-wrapper">
+        <input 
+          type={type === "password" && showPassword ? "text" : type} 
+          id={id} 
+          name={name} 
+          required 
+          onChange={handleInputChange} 
+        />
+        {type === "password" && isPasswordVisible && (
+          <span className="eye-icon" onClick={() => setShowPassword(!showPassword)}>
+            {showPassword ? <Eye size={20} /> : <EyeOff size={20} />}
+          </span>
+        )}
+      </div>
     </div>
-  );
-
-  // Обновили текст ссылок в заголовке
+    );
+  };
+    
   const FormHeader = ({ isLoginMode }) => (
     <div className="box form">
       <div className="text">
         <p>
-          <span className={isLoginMode ? "highlight" : ""} onClick={() => setIsLoginMode(true)}>Вход</span> / 
-          <span className={!isLoginMode ? "highlight" : ""} onClick={() => setIsLoginMode(false)}>Регистрация</span>
+          <span className={!isLoginMode ? "highlight" : ""} onClick={() => setIsLoginMode(false)}>Регистрация</span> / 
+          <span className={isLoginMode ? "highlight" : ""} onClick={() => setIsLoginMode(true)}>Вход</span>
         </p>
       </div>
     </div>
   );
 
-  // Обновили текст ссылок
   const FormLinks = ({ isLoginMode }) => (
     <div className="form-links">
       <a onClick={() => setIsLoginMode(!isLoginMode)}>
         {isLoginMode ? "Ещё не зарегистрированы?" : "Уже зарегистрированы?"}
       </a>
       <a onClick={() => setIsLoginMode(!isLoginMode)}>
-        {isLoginMode ? "Создать аккаунт" : "Войти в аккаунт"}
+        {isLoginMode ? "Регистрация" : "Вход"}
       </a>
     </div>
   );
 
-  // Поменяли порядок рендеринга форм
-  const BlockForms = ({ isLoginMode }) => (
-    <div className="block-of-forms">
-      <BackgroundTransition />
-      <FormHeader isLoginMode={isLoginMode} />
-      {isLoginMode ? <LoginForm /> : <RegistrationForm />}
+  const RegistrationForm = () => (
+    <div className="box form-1">
+      <form className="form-content" action="path_to_your_processing_script" method="POST">
+        <InputField label="Почта" type="email" id="email" name="email" />
+        <InputField label="Логин" type="text" id="username" name="username" />
+        <InputField label="Пароль" type="password" id="password" name="password" />
+        <FormLinks isLoginMode={isLoginMode} />
+      </form>
     </div>
   );
 
-  // Форма входа
   const LoginForm = () => (
     <div className="box form-1">
       <form className="form-content" action="path_to_your_processing_script" method="POST">
@@ -62,15 +86,11 @@ const AuthenticationAuthorizationPage = () => {
     </div>
   );
 
-  // Форма регистрации
-  const RegistrationForm = () => (
-    <div className="box form-1">
-      <form className="form-content" action="path_to_your_processing_script" method="POST">
-        <InputField label="Почта" type="email" id="email" name="email" />
-        <InputField label="Логин" type="text" id="username" name="username" />
-        <InputField label="Пароль" type="password" id="password" name="password" />
-        <FormLinks isLoginMode={isLoginMode} />
-      </form>
+  const BlockForms = ({ isLoginMode }) => (
+    <div className="block-of-forms">
+      <BackgroundTransition />
+      <FormHeader isLoginMode={isLoginMode} />
+      {isLoginMode ? <LoginForm /> : <RegistrationForm />}
     </div>
   );
 
