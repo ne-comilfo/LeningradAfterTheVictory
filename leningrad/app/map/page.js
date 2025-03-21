a93a33d2540666e506689e5d3c34ae39ac8bfdcb
Григорий Малышев <grisha@192-168-0-101.local>
2025-03-21 20:56:17 +0300

    feat: now year filters do not overlap when they are nearby, Gatchina and Peterhof are also visible on the map

--- leningrad/app/map/page.js
+++ leningrad/app/map/page.js
@@ -9,8 +9,6 @@ import './/page-style.css';
 import { useRouter } from 'next/navigation';
 import InfoWindow from '../info-for-map/page';
 
-
-
 export default function Map() {
     const [markers, setMarkers] = useState([]);
     const [filteredMarkers, setFilteredMarkers] = useState([]);
@@ -18,15 +16,16 @@ export default function Map() {
     const [endYear, setEndYear] = useState(null);
     const [maxYear, setMaxYear] = useState(null);
     const [minYear, setMinYear] = useState(null);
-    const [selectedMarker, setSelectedMarker] = useState(null); // Выбранная метка
-    const [isInfoWindowOpen, setIsInfoWindowOpen] = useState(false); // Открыто ли окно
+    const [selectedMarker, setSelectedMarker] = useState(null);
+    const [isInfoWindowOpen, setIsInfoWindowOpen] = useState(false);
     const [isExpanded, setIsExpanded] = useState(false);
     const router = useRouter();
+
+    const MIN_YEAR_DIFFERENCE = 9; // Минимальная разница между годами
 
     const clearRoute = () => {
         if (!map.current) return;
 
-        // Удаляем слой и источник маршрута, если они существуют
         if (map.current.getLayer('route')) {
             map.current.removeLayer('route');
             map.current.removeSource('route');
@@ -36,13 +35,11 @@ export default function Map() {
     const drawRoute = (coordinates) => {
         if (!map.current || !coordinates || coordinates.length === 0) return;
 
-        // Удаляем предыдущий маршрут, если он был
         if (map.current.getLayer('route')) {
             map.current.removeLayer('route');
             map.current.removeSource('route');
         }
 
-        // Создаем GeoJSON объект для маршрута
         const geojson = {
             type: 'Feature',
             properties: {},
@@ -52,7 +49,6 @@ export default function Map() {
             },
         };
 
-        // Добавляем источник и слой для маршрута
         map.current.addSource('route', {
             type: 'geojson',
             data: geojson,
@@ -72,7 +68,6 @@ export default function Map() {
             },
         });
 
-        // Масштабируем карту так, чтобы маршрут был виден
         const bounds = coordinates.reduce((bounds, coord) => {
             return bounds.extend(coord);
         }, new maptilersdk.LngLatBounds(coordinates[0], coordinates[0]));
@@ -82,7 +77,6 @@ export default function Map() {
         });
     };
 
-
     const mapContainer = useRef(null);
     const map = useRef(null);
     const spb = { lng: 30.3148, lat: 59.9343 };
@@ -90,17 +84,15 @@ export default function Map() {
     const lang = 'ru';
     maptilersdk.config.apiKey = 'ozVhpb986qzcqh7JCGLE';
 
-
-    //  Ссылки на метки для отображения
     const markerElements = useRef([]);
+
     useEffect(() => {
         const fetchMarkers = async () => {
             try {
                 const response = await fetch("http://194.87.252.234:6060/api/attractions/get-all");
                 const data = await response.json();
-                setMarkers(data); // Обновляем состояние с метками
+                setMarkers(data);
 
-                // Определяем минимальный и максимальный год
                 const years = data.map(marker => marker.yearOfCreation);
                 setMinYear(Math.min(...years));
                 setMaxYear(Math.max(...years));
@@ -114,19 +106,14 @@ export default function Map() {
         fetchMarkers();
     }, []);
 
-
-
     useEffect(() => {
-        // Начальные значения слайдера
         setStartYear(minYear);
         setEndYear(maxYear);
     }, [minYear, maxYear]);
 
     useEffect(() => {
-
         if (!mapContainer.current || map.current) return;
 
-        
         map.current = new maptilersdk.Map({
             container: mapContainer.current,
             style: maptilersdk.MapStyle.STREETS,
@@ -136,13 +123,11 @@ export default function Map() {
         });
 
         const bounds = [
-            [29.8, 59.7], // Юго-западная граница (нижний левый угол)
-            [30.8, 60.1], // Северо-восточная граница (верхний правый угол)
+            [29.6, 59.5],
+            [30.8, 60.1],
         ];
         map.current.setMaxBounds(bounds);
-    
-        // Устанавливаем минимальный и максимальный уровень масштабирования
-        map.current.setMinZoom(10); // Минимальный уровень масштабирования
+        map.current.setMinZoom(10);
         map.current.setMaxZoom(14);
 
         return () => {
@@ -153,13 +138,10 @@ export default function Map() {
         };
     }, []);
 
-
-
     useEffect(() => {
-        // Фильтрация меток по годам
         if (startYear !== null && endYear !== null) {
             const filtered = markers.filter(marker => {
-                const buildYear = marker.yearOfCreation; // Год постройки здания
+                const buildYear = marker.yearOfCreation;
                 return buildYear >= startYear && buildYear <= endYear;
             });
             setFilteredMarkers(filtered);
@@ -167,62 +149,25 @@ export default function Map() {
     }, [startYear, endYear, markers]);
 
     const handleChange = (value) => {
-        // Обновляем диапазон по клику на фильтр
-        setStartYear(value[0]);
-        setEndYear(value[1]);
+        const [newStartYear, newEndYear] = value;
+
+        if (newEndYear - newStartYear < MIN_YEAR_DIFFERENCE) {
+            return;
+        }
+
+        setStartYear(newStartYear);
+        setEndYear(newEndYear);
     };
 
     const calculatePosition = (value) => {
-        // Рассчитываем процентное положение для текущего значения ручки слайдера
         return ((value - minYear) / (maxYear - minYear)) * 92;
     };
 
-    useEffect(() => {
-        if (!map.current) return; // Проверяем, что карта была инициализирована
+    const renderYearLabels = () => {
+        const difference = endYear - startYear;
 
-        // Очистка текущих меток, добавленных на карту
-        markerElements.current.forEach(marker => marker.remove());
-        markerElements.current = [];
-
-        // Добавление новых меток на карту
-        filteredMarkers.forEach(marker => {
-            if (marker?.location?.coordinates) { // Проверяем, что координаты существуют
-                const markerElement = new maptilersdk.Marker()
-                    .setLngLat(marker.location.coordinates)
-                    .addTo(map.current);
-
-                markerElement.getElement().addEventListener('click', () => {
-                    setSelectedMarker(marker);
-                    setIsInfoWindowOpen(true);
-                    setIsExpanded(false);
-                });
-
-                markerElements.current.push(markerElement);
-            }
-        });
-    }, [filteredMarkers]);
-
-
-
-    return (
-        <div>
-            <div className="map-wrap">
-                <div ref={mapContainer} className="map" />
-            </div>
-
-            <div className="slider-container">
-
-                <div>
-                    <Slider
-                        range
-                        min={minYear}
-                        max={maxYear}
-                        value={[startYear, endYear]}
-                        onChange={handleChange}
-                    />
-                </div>
-
-
+        if (difference === MIN_YEAR_DIFFERENCE) {
+            return (
                 <div className="slider-labels">
                     <span className="min-label">{minYear}</span>
                     <span className="max-label">{maxYear}</span>
@@ -245,25 +190,94 @@ export default function Map() {
                     >
                         {endYear}
                     </span>
+                    
                 </div>
+            );
+        }
+
+        return (
+            <div className="slider-labels">
+                <span className="min-label">{minYear}</span>
+                <span className="max-label">{maxYear}</span>
+
+                <span
+                    className="current-label start-label"
+                    style={{
+                        left: `${calculatePosition(startYear) + 4}%`,
+                        transform: 'translateX(-50%)',
+                    }}
+                >
+                    {startYear}
+                </span>
+                <span
+                    className="current-label end-label"
+                    style={{
+                        left: `${calculatePosition(endYear) + 4}%`,
+                        transform: 'translateX(-50%)',
+                    }}
+                >
+                    {endYear}
+                </span>
             </div>
+        );
+    };
+
+    useEffect(() => {
+        if (!map.current) return;
+
+        markerElements.current.forEach(marker => marker.remove());
+        markerElements.current = [];
+
+        filteredMarkers.forEach(marker => {
+            if (marker?.location?.coordinates) {
+                const markerElement = new maptilersdk.Marker()
+                    .setLngLat(marker.location.coordinates)
+                    .addTo(map.current);
+
+                markerElement.getElement().addEventListener('click', () => {
+                    setSelectedMarker(marker);
+                    setIsInfoWindowOpen(true);
+                    setIsExpanded(false);
+                });
+
+                markerElements.current.push(markerElement);
+            }
+        });
+    }, [filteredMarkers]);
+
+    
+
+    return (
+        <div>
+            <div className="map-wrap">
+                <div ref={mapContainer} className="map" />
+            </div>
+
+            <div className="slider-container">
+                <div>
+                    <Slider
+                        range
+                        min={minYear}
+                        max={maxYear}
+                        value={[startYear, endYear]}
+                        onChange={handleChange}
+                    />
+                </div>
+
+                {renderYearLabels()}
+            </div>
+
             {isInfoWindowOpen && (
                 <InfoWindow
-                    marker={selectedMarker} // Передаем выбранную метку
-                    onClose={() => setIsInfoWindowOpen(false)} // Закрываем окно
-                    isExpanded={isExpanded} // Передаем состояние свернуто/развернуто
-                    setIsExpanded={setIsExpanded} // Передаем функцию для управления состоянием
+                    marker={selectedMarker}
+                    onClose={() => setIsInfoWindowOpen(false)}
+                    isExpanded={isExpanded}
+                    setIsExpanded={setIsExpanded}
                     drawRoute={drawRoute}
                     clearRoute={clearRoute}
                     map={map}
-
                 />
             )}
         </div>
-
     );
-
 }
-
-
-
