d83f10004914886cc3e54e1efc7205a4d907bcb6
Григорий Малышев <grisha@MacBook-Air-Grigorij.local>
2025-04-04 21:05:32 +0300

    fix: added swiper library

--- leningrad/next.config.mjs
+++ leningrad/next.config.mjs
@@ -1,8 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-        missingSuspenseWithCSRBailout: false, // Отключает ошибку с Suspense
-      },
+  experimental: {
+    missingSuspenseWithCSRBailout: false, // Отключает ошибку с Suspense
+  },
+
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+
+      'swiper/react': 'swiper/react/swiper-react',
+      'swiper/modules': 'swiper/modules',
+      'swiper': 'swiper/swiper-bundle.min.js',
+    };
+
+    return config;
+  },
 };
 
 export default nextConfig;
